72f22f93933d2c2f23f871e5835cc7d8cd7987d3
Alejo.Martinez <100450617+Alejolz@users.noreply.github.com>
2024-05-30 21:16:11 -0500

    Feat: first implementation of user registration

---
+++ handlers/register.js
@@ -0,0 +1,103 @@
+/**  
+ * |------------------------------------------------------|
+ * |@function         |@version |@date                    |
+ * |                  |         |                         |
+ * |------------------------------------------------------| 
+*/
+
+const mysqlDB = require('../utils/sql');
+const crypto = require('crypto');
+
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+function createResponse(statusCode, message) {
+    return {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(message)
+    };
+}
+
+function isNullOrEmpty(obj) {
+    return obj === undefined || obj === null || obj == NaN || obj === '';
+}
+
+function findMissingObligatoryFields(fields, obligatoryFields) {
+    return obligatoryFields.filter(field => !fields.includes(field));
+}
+
+function encrypt(password) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(password);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return encrypted.toString('hex');
+
+}
+exports.handler = async (event, context, callback) => {
+
+    const connectionConfig = {
+        host: 'monorail.proxy.rlwy.net',
+        user: 'root',
+        password: 'pYsEeaEJbAWyAUBDStfstBKVXyRdOrtK',
+        database: 'railway',
+        port: '27406'
+    };
+
+    let connection;
+
+    try {
+
+        connection = await mysqlDB.createConnection(connectionConfig);
+        let user = JSON.parse(event.body);
+        let obligatoryFields = ['name', 'username', 'password', 'email']
+
+        if (!isNullOrEmpty(user)) {
+
+            const tableExistsResult = await mysqlDB.tableExists(connection, 'APRENDICES');
+            if (!tableExistsResult) {
+                return createResponse(404, { message: 'La tabla APRENDICES no existe en la base de datos' });
+            }
+
+            if (user.password.trim().length > 20) {
+                return createResponse(400, { message: 'La contraseña no puede tener más de 20 caracteres' });
+            }
+            let existUserName = await mysqlDB.userExistsInDB(connection, user.username);
+            if (existUserName) {
+                return createResponse(400, { message: 'El usuario ya existe' });
+            }
+
+            let missingFields = findMissingObligatoryFields(Object.getOwnPropertyNames(user), obligatoryFields);
+
+            if (missingFields.length > 0) {
+                return createResponse(400, { message: 'Faltan campos obligatorios: ' + missingFields.join(', ') });
+            }
+
+
+        } else {
+
+            return createResponse(400, { message: 'No se encontro body en el request enviado' });
+        }
+
+        console.log('Paso validaciones')
+
+        const userDB = await mysqlDB.createUser(connection, user);
+
+        if (userDB) {
+            return createResponse(200, { message: 'Usuario creado exitosamente' });
+        } else {
+            return createResponse(400, { message: 'Error al crear el usuario' });
+        }
+
+    } catch (e) {
+        console.error('Error durante la conexión a la base de datos o procesamiento:', e);
+        return createResponse(500, { message: 'Error interno del servidor al ejecutar' });
+    } finally {
+        if (connection) {
+            await connection.end();
+        }
+    }
+};
--- utils/sql.js
+++ utils/sql.js
@@ -20,3 +20,13 @@ module.exports.userExists = async (connection, username, password) => {
     const [rows] = await connection.execute('SELECT * FROM APRENDICES WHERE USER_NAME = ? AND PASSWORD = ?', [username, password]);
     return rows.length > 0;
 };
+
+module.exports.userExistsInDB = async (connection, username) => {
+    const [rows] = await connection.execute('SELECT * FROM APRENDICES WHERE USER_NAME = ?', [username]);
+    return rows.length > 0;
+}
+
+module.exports.createUser = async (connection, user) => {
+    const [rows] = await connection.execute('INSERT INTO APRENDICES (NAME, USER_NAME, PASSWORD, EMAIL) VALUES (?, ?, ?, ?)', [user.name, user.username, user.password, user.email]);
+    return rows.affectedRows > 0;
+}
